7fc90f9702a8a8a23576736c1348f511ba826e85
unknown <usndangol97@gmail.com>
2020-02-15 22:27:56 +0545

    Improvement in mid square method.

--- Mid_square.cpp
+++ Mid_square.cpp
@@ -1,19 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int checknum(int arr[],int i,int flag)
+{
+    for(int j=0;j<i;j++)
+    {
+        if(arr[i]==arr[j])
+        {
+            flag=1;
+            break;
+        }
+    }
+    return flag;
+}
 int main()
 {
-    int seed,seed_sq,next_no,n;
+    int seed[50],seed_sq,next_no,i=0,flag=0;
     printf("Enter the Seed:");
-    scanf("%d",&seed);
-    printf("Enter no of Random digits you want:");
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
+    scanf("%d",&seed[i]);
+    do
     {
-        seed_sq=seed*seed;
+        seed_sq=seed[i]*seed[i];
         next_no=(seed_sq%1000)/10;
-        seed=next_no;
-        printf("=>%d\n",seed);
-    }
+        seed[i+1]=next_no;
+        printf("=>%d\n",seed[i]);
+        i++;
+        flag=checknum(seed,i,flag);
+    }while(flag==0);
 }
 
